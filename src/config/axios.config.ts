56846b48fc5f796f52b3f315e8933df030e91927
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2024-12-05 11:24:04 -0300

    refactor: URL for DEPLOY

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}:${process.env.NEXT_PUBLIC_PORT}`,
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
   headers: { "Content-Type": "application/json" },
 });
 
